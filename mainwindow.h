2dc317d0a57202df55cb7f79e6b3fb82f820bb0f
Derkach Evgeniy <my,yami.ua@gmail.com>
2015-05-31 23:10:26 +0300

    Revert "Derkach Evgeniy"
    This reverts commit b8a7211f9938e4557ba4e3a5e4da552118c6856a.

--- mainwindow.h
+++ mainwindow.h
@@ -26,10 +26,10 @@ private slots:
     void on_buttonDialog2_clicked();
     void on_buttonDialog4_clicked();
     void on_buttonDialog5_clicked();
-    void on_buttonDialog7_clicked();
-    void on_buttonDialog8_clicked();
+    void on_buttonDialog6_clicked();
     void on_buttonDialog9_clicked();
     void on_buttonDialog10_clicked();
+    void on_buttonDialog11_clicked();
     void on_buttonDialog3_clicked();
 };
 
