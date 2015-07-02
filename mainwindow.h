dfa1f343815a308e5625e9525c9568f857e5bbe7
artyrio <dark_king@ukr.net>
2015-07-02 19:15:38 +0300

    Revert "Pidpleta"
    This reverts commit dd5d4dd71861fd4637cf4daecbb4e658a91e07b9.

--- mainwindow.h
+++ mainwindow.h
@@ -15,11 +15,24 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+protected:
+    void changeEvent(QEvent *e);
+
 private:
     Ui::MainWindow *ui;
 
 private slots:
-    void changeText();
+    void on_buttonDialog1_clicked();
+    void on_buttonDialog2_clicked();
+    void on_buttonDialog4_clicked();
+    void on_buttonDialog5_clicked();
+    void on_buttonDialog6_clicked();
+    void on_buttonDialog9_clicked();
+    void on_buttonDialog10_clicked();
+    void on_buttonDialog11_clicked();
+    void on_buttonDialog3_clicked();
+    void on_buttonDialog8_clicked();
+    void on_buttonDialog7_clicked();
 };
 
 #endif // MAINWINDOW_H
