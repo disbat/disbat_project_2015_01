51a2fb94a68ff10bd7b11933142ef1b6d158e6c8
stanislav1995 <shved95@gmail.com>
2015-09-07 18:51:22 +0300

    Dialog13

--- dialog13.cpp
+++ dialog13.cpp
@@ -15,17 +15,48 @@ Dialog13::~Dialog13()
 }
 
 
-void MainWindow::on_pushButton_clicked()
+void Dialog13::on_pushButton_1_clicked()
 {
-    ui->pushButton->hide();
+    ui->pushButton_1->hide();
 }
 
-void MainWindow::on_pushButton_2_clicked()
+void Dialog13::on_pushButton_2_clicked()
 {
     ui->pushButton_2->hide();
 }
 
-void MainWindow::on_pushButton_3_clicked()
+void Dialog13::on_pushButton_3_clicked()
 {
     ui->pushButton_3->hide();
 }
+
+void Dialog13::on_pushButton_4_clicked()
+{
+    ui->pushButton_4->hide();
+}
+
+void Dialog13::on_pushButton_5_clicked()
+{
+    ui->pushButton_5->hide();
+}
+
+void Dialog13::on_pushButton_6_clicked()
+{
+    ui->pushButton_6->hide();
+}
+
+void Dialog13::on_pushButton_7_clicked()
+{
+    ui->pushButton_7->hide();
+}
+
+void Dialog13::on_pushButton_8_clicked()
+{
+    ui->pushButton_8->hide();
+}
+
+void Dialog13::on_pushButton_9_clicked()
+{
+    ui->pushButton_9->hide();
+}
+
